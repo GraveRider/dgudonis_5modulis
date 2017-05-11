9f4f864764b696506ef94e510f34335fb605464c
Grave Rider <donprayer@gmail.com>
2017-05-11 14:32:11 +0300

    Added method for handling user's input

--- 3 uzduotis/src/Main/TemperatureCalculator.java
+++ 3 uzduotis/src/Main/TemperatureCalculator.java
@@ -1,16 +1,33 @@
 package Main;
 
+import java.util.Scanner;
+
 /**
  * Converts user's entered temperature from Kelvin to Celsius, Fahrenheit & Reaumur scales.
+ *
  * @author Grave
  * @version 1.0
  */
 public class TemperatureCalculator {
 
     public static void main(String[] args) {
-        System.out.println(calculateCelsius(310.94));
-        System.out.println(calculateFahrenheit(310.94));
-        System.out.println(calculateReaumur(310.94));
+        start();
+    }
+
+    public static double doubleInput(Scanner reader) {
+        double number = 0;
+        while (true) {
+            try {
+                number = Double.parseDouble(reader.nextLine());
+                if (true) {
+                    break;
+                }
+            } catch (IllegalArgumentException e) {
+                System.err.println("Incompatible type! " + e.getMessage());
+            }
+        }
+        reader.close();
+        return number;
     }
 
     public static double calculateCelsius(double kelvin) {
@@ -23,5 +40,16 @@ public class TemperatureCalculator {
 
     public static double calculateReaumur(double kelvin) {
         return Math.round(((kelvin - 273.15) * 4 / 5) * 100) / 100D;
+    }
+
+    public static void start() {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Įveskite temperatūra kelvino skalėje: ");
+        double kelvin = doubleInput(reader);
+
+        System.out.println(calculateCelsius(kelvin) + " Celsijaus laipsnių");
+        System.out.println(calculateFahrenheit(kelvin) + " Farenheito laipsnių");
+        System.out.println(calculateReaumur(kelvin) + " Reomiūro laipsnių");
     }
 }
